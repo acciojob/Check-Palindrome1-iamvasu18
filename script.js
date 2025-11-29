be3aca2a0a2bae5593857c81e8fb8455d1d8805c
Dharavath Vasu Naik <168115062+iamvasu18@users.noreply.github.com>
2025-11-29 23:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(str){	
+	let rev=str.split(" ").reverse().join(" ");
+	return rev===str;
 }
 module.exports = palindrome
